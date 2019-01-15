f2a682631470c9ed23f65e938d6ee3ced83e87c2
Dass987 <sequeradanniel@gmail.com>
2019-01-15 17:02:23 -0400

    Products storage v1.1

--- index.py
+++ index.py
@@ -32,12 +32,24 @@ class Product:
 		self.button = ttk.Button(frame, text="Save Product", command = self.add_product)
 		self.button.grid(row = 3, columnspan = 2, sticky = W + E)
 		
+		# Output message
+		self.message = Label(text="", fg="red")
+		self.message.grid(row = 3, column = 0, columnspan = 2, sticky = W + E)
+
 		# Table
 		self.tree = ttk.Treeview(height = 10, columns=2)
 		self.tree.grid(row = 4, column = 0, columns = 2)
 		self.tree.heading("#0", text="Name", anchor=CENTER)
 		self.tree.heading("#1", text="Price", anchor=CENTER)
 
+		# Table buttons
+		self.deleteButton = ttk.Button(text = "Delete", command = self.delete_product)
+		self.deleteButton.grid(row = 5, column = 0, sticky = W + E)
+		
+		self.updateButton = ttk.Button(text = "Edit")
+		self.updateButton.grid(row = 5, column = 1, sticky = W + E)
+
+		# Filling table rows
 		self.get_products()
 
 	def run_query(self, query, parameters=()):
@@ -63,16 +75,37 @@ class Product:
 		for row in db_rows:
 			self.tree.insert('', 0, text=row[1], values="$" + str(row[2]))
 
+	# Validate user entry
 	def validation(self):
 		return len(self.name.get()) != 0 and len(self.price.get()) != 0
-		
+	
+	# Add new product
 	def add_product(self):
 		
 		if self.validation():
-			print(self.name.get())
-			print(self.price.get())
+			
+			query = "INSERT INTO product VALUES(NULL, ?, ?)"
+			parameters = (self.name.get(), self.price.get())
+			self.run_query(query, parameters)
+			self.message["text"] = "Product {} added successfully".format(self.name.get())
+
+			self.name.delete(0, END)
+			self.price.delete(0, END)
+
 		else:
-			print('Name and price are required')
+
+			self.message["text"] = "Name and Price are required"
+		
+		self.get_products()
+	
+	# Delete product
+	def delete_product(self):
+			
+		try:
+			self.tree.item(self.tree.selection())["text"]
+		except IndexError as e:
+			self.message['text'] = 'Please Select a Record'
+			return
 
 if __name__ == '__main__':
 	window = Tk()
